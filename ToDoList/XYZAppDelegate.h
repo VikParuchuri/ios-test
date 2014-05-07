3997c6058b1bb093f33b9e6e7ad827218f257043
VP <vik@VPs-Mac.local>
2014-05-07 06:54:03 -0700

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by VP on 5/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
